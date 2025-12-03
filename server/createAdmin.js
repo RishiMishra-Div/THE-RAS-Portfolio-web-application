437a39dc91ad54e6e4d24d478350e768b4bc4a84
Rishi Mishra <rishimishra9615@gmail.com>
2025-12-03 18:24:31 +0530

    Update createAdmin.js

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -1,13 +1,1 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-require("dotenv").config();
-const Admin = require("./models/adminModel");
 
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const passwordHash = await bcrypt.hash("theras96@#", 10);
-  await Admin.create({ email: "admin@gmail.com", password: passwordHash });
-  console.log("Admin created!");
-  process.exit();
-});
-
-module.exports = mongoose.Connection;
